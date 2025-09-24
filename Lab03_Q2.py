b2b7eaa3b9ee7890a53a2bd3946ca45d80863724
Zachary Klouchnikov <zachary.klou@gmail.com>
2025-09-24 16:22:30 -0400

    Began question 2 part a)

--- Lab03_Q2.py
+++ Lab03_Q2.py
@@ -1,1 +1,40 @@
 __authors__ = "Zachary Klouchnikov and Hannah Semple"
+
+"""
+IMPORTS
+"""
+import numpy as np
+
+from gaussxw import gaussxw
+
+"""
+FUNCTIONS
+"""
+
+"""
+PART A)
+"""
+"Constants"
+K = 12.0
+M = 1.0
+X_0 = 0.001
+
+"Stuff"
+g = lambda x: 4 / np.sqrt(K * (X_0 ** 2 - x ** 2))
+
+n = 8
+a = 0.0
+b = X_0
+
+# Calculate the sample points and weights, then map them to the required
+# integration domain
+x, w = gaussxw(n)
+xp = 0.5 * (b - a) * x + 0.5 * (b + a)
+wp = 0.5 * (b - a) * w
+
+# Perform the integration
+s = 0.0
+for k in range(n):
+    s += wp[k] * g(xp[k])
+
+print(s)
