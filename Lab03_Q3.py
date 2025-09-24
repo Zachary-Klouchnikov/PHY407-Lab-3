37651986ea89c479d4b87a6b308e835aa4bb6b19
hannahsemple4 <hannahsemple4@gmail.com>
2025-09-24 13:19:51 -0400

    format

--- Lab03_Q3.py
+++ Lab03_Q3.py
@@ -1,1 +1,27 @@
 __authors__ = "Zachary Klouchnikov and Hannah Semple"
+
+# HEADER
+
+"""
+IMPORTS
+"""
+
+"""
+FUNCTIONS
+"""
+
+"""
+PART A
+"""
+
+"""
+PART B
+"""
+
+"""
+PART C
+"""
+
+"""
+PART D
+"""
